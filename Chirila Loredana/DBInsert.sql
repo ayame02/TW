7fbdd81c9bd5939639848e1f733a4f4e93d11e5e
Chirila Loredana <cute.niiko@gmail.com>
2017-06-07 08:09:38 -0500

    Create DBInsert.sql

---
+++ Chirila Loredana/DBInsert.sql
@@ -0,0 +1,8 @@
+INSERT INTO users(id, username, password) VALUES (100, "loredana.chirila", "yaoilover");
+/
+INSERT INTO users(id, username, password) VALUES (101, "alexandru.agape", "cookie");
+/
+INSERT INTO users(id, username, password) VALUES (102, "samuel.gavriliuc", "cars");
+/
+INSERT INTO users(id, username, password) VALUES (103, "cristina.belcescu", "orange");
+/
