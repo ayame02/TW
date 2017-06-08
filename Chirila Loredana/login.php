5ffbb292ec85bc776becece86f9310400cf9d1e1
Chirila Loredana <cute.niiko@gmail.com>
2017-06-08 03:35:40 +0300

    Add files via upload

--- Chirila Loredana/login.php
+++ Chirila Loredana/login.php
@@ -7,17 +7,24 @@
 		<div id="redirecting_content">	
 		<h2>
 		<?php
-			include ('code.php');
-			
+			session_start();
+			session_id('home');
+			include ('databaseConn.php');
+			$_SESSION['user'] = $_POST['user'];
 			$db = new Database();
-
-			if ($db->users_check($_POST["user"], $_POST["pass"]) === 1){
-				
-				echo ("Login success...Redirecting.");
-				
+			if (isset($_POST["user"]) and isset($_POST["pass"])){
+				if ($db->users_check($_POST["user"], $_POST["pass"]) === 1){
+					echo ("Login success...Redirecting.");
+				}
+				else {
+					echo ("Could not login, please try again later.");
+					header('Refresh: 2; URL=Home.html');
+					die();
+				}
 			}
 			else {
-				header('Location: ReturnHome.html');
+				echo ("Invalid user and/or password. Redirecting.");
+				header ('Refresh: 2; URL=Home.html');
 				die();
 			}
 		?>
