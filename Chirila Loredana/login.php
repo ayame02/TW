46ace618ffab1a0465f65ce3953526d3b58f7da3
Chirila Loredana <cute.niiko@gmail.com>
2017-06-19 11:29:59 -0500

    Update login.php

--- Chirila Loredana/login.php
+++ Chirila Loredana/login.php
@@ -1,6 +1,6 @@
 <html>
 	<head>
-		<meta http-equiv = "refresh" content = "2; url = NaturalHome.html">
+		<meta http-equiv = "refresh" content = "2; URL=NaturalHome.html">
 		<link rel="stylesheet" type="text/css" href="css/style.css" />
 	</head>
 	<body>
@@ -9,23 +9,39 @@
 		<?php
 			session_start();
 			include ('databaseConn.php');
-			$_SESSION['user'] = $_POST['user'];
+			$_SESSION['user_id'] = $_POST['user_id'];
 			$db = new Database();
-			if (isset($_POST["id"])){
-				if ($db->users_check($_POST["id"]) === 1){
+			$value = $_POST['user_id'];
+			if (isset($_POST['user_id'])){
+				if ($db->users_check($value) === 1){
 					echo ("Login success...Redirecting.");
+					header ('Refresh: 2; URL=NaturalHome.html');
 				}
 				else {
-					echo ("Could not login, please try again later.");
-					header('Refresh: 2; URL=Home.html');
-					die();
+					if( $value > 100000 || $value < 999999) {
+						$new_user = "insert into users(user_id) values($value)";
+						$result = $db->query($new_user);
+						if (!$result) { 
+							echo ("Cannot complete action. Please try again later."); 
+							header ('Refresh: 2; URL=Home.html');
+						}
+						else{
+							echo("Success! Redirecting to main page.");
+							header ('Refresh: 2; URL=NaturalHome.html');
+						}
+					}
+					else {
+						echo ("Invalid id. Redirecting."); 
+						header ('Refresh: 2; URL=Home.html');
+					}
 				}
 			}
 			else {
-				echo ("Invalid user and/or password. Redirecting.");
+				echo ("Invalid id. Redirecting.");
 				header ('Refresh: 2; URL=Home.html');
-				die();
+				
 			}
+			
 		?>
 		</h2>
 		</div>
